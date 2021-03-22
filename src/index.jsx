54ca1b669423b154c5e1580f3a2d7a8e9bf1a989
Anton Zinchenko <d-krabike@yandex.ru>
2021-03-22 14:57:44 +0500

    feat: add redux-persistor

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { store } from "./reducers";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import App from "./components/App";
-import "./style/global.less";
+import Store from './reducers';
+import App from './components/App';
+import './style/global.less';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
+const { store, persistor } = Store();
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   rootElement
 );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,26 @@
-import { combineReducers } from "redux";
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
+import { combineReducers } from 'redux';
+import { createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import storage from 'redux-persist/lib/storage';
 
-import phoneReducers from "./phoneReducers";
+import phoneReducers from './phoneReducers';
 
 const rootReducer = combineReducers({
   phone: phoneReducers,
 });
 
-export const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+const persistConfig = {
+  key: 'phone',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(rootReducer);
+
+export default () => {
+  let store = createStore(persistedReducer, composeWithDevTools());
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
